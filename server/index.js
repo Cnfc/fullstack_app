d48c4cf77aed6cff95f3f2aa809e9ed0f3b2ab77
Cnfc <stdashkov@mail.ru>
2019-03-23 21:46:54 +0300

    add keys in ignore.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({whatsUp: 'FineFineFine'});
-});
+
+passport.use(new GoogleStrategy());
 
 
 
