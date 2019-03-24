fd7b75197804398e2f4a8652ae9ba3ecfc31098f
Cnfc <stdashkov@mail.ru>
2019-03-24 20:46:00 +0300

    add mongo and mongoose & createSchema

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./service/passport');
+require('./modules/User');
+
 const authRoutes = require('./routes/authRoutes');
+
+// Connecting DB
+mongoose.connect(keys.mongoURL);
 
 // Init
 const app = express();
@@ -11,6 +17,7 @@ const app = express();
 
 // Routes
 authRoutes(app);
+
 
 
 const PORT = process.env.PORT || 5000
