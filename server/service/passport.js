48e73db0517a94b5567b34c7f440146cfbc888ad
Cnfc <stdashkov@mail.ru>
2019-03-25 17:17:57 +0300

    del & detect the same googleID

--- server/service/passport.js
+++ server/service/passport.js
@@ -12,7 +12,18 @@ passport.use(
     callbackURL: "/auth/google/callback"
   }, 
   (accessToken, refreshToken, profile, done) => {
-    new User({ googleId: profile.id }).save();
+    // Detect the SAME id
+    User.findOne({googleId: profile.id})
+    .then((existingUser) => {
+      if(existingUser) {
+        // We already have a record profile ID
+      } else {
+        // We don`t have profile ID
+        new User({ googleId: profile.id }).save();
+      }
+    })
+
+    
   }
   )
 );
