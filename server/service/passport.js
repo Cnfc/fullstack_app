64c9ec6a2f66505393e1c2d9464078933984c0eb
Cnfc <stdashkov@mail.ru>
2019-03-26 20:39:20 +0300

    change relative part

--- server/service/passport.js
+++ server/service/passport.js
@@ -21,7 +21,7 @@ passport.use(
   new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: "/auth/google/callback",
+    callbackURL: "https://mighty-ocean-47050.herokuapp.com/auth/google/callback",
     proxy: true
   }, 
   (accessToken, refreshToken, profile, done) => {
