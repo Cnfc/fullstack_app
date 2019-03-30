cfa3536ddc159b40e8a6ef929e10eaef4a21716c
Cnfc <stdashkov@mail.ru>
2019-03-30 12:40:47 +0300

    add index frontend

--- server/service/passport.js
+++ server/service/passport.js
@@ -24,19 +24,17 @@ passport.use(
     callbackURL: "/auth/google/callback",
     proxy: true
   }, 
-  (accessToken, refreshToken, profile, done) => {
+  async (accessToken, refreshToken, profile, done) => {
     // Detect the SAME id
-    User.findOne({googleId: profile.id}).then((existingUser) => {
-      if(existingUser) {
-        // We already have a record profile ID
-        done(null, existingUser);
-      } else {
-        // We don`t have profile ID
-        new User({ googleId: profile.id })
-        .save()
-        .then(user => done(null, user));
-      }
-    });   
+    const existingUser = await User.findOne({googleId: profile.id});
+
+    if(existingUser) {
+      // We already have a record profile ID
+      return done(null, existingUser);
+    } 
+    // We don`t have profile ID
+    const user =  await new User({ googleId: profile.id }).save()
+    done(null, user);
   }
   )
 );
