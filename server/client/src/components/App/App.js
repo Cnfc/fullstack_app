f475a01972a27b4f69e093432625735bb4c92fa8
Cnfc <stdashkov@mail.ru>
2019-04-03 17:21:45 +0300

    add action

--- server/client/src/components/App/App.js
+++ server/client/src/components/App/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import './App.css';
+import { connect } from 'react-redux';
+import * as actions from '../../action/types';
 
+import './App.css';
 import Header from '../Header';
 import Dashboard from '../Dashboard';
 import SurveyNew from '../SurveyNew';
@@ -9,6 +11,11 @@ import Landing from '../Landing';
 
 
 class App extends Component {
+
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+
   render() {
     return (
       <div className="container">
@@ -24,5 +31,5 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(null, actions)(App);
 
--- server/client/src/reducer/authReducer.js
+++ server/client/src/reducer/authReducer.js
@@ -1,8 +1,9 @@
 export default (state = {}, action) => {
+  console.log(action);
   switch (action.type) {
     case 'LOGGED_IN':
       return {
-        
+
       }
     default: 
       return state;
